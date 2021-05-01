abb64daa77112365c302179cf304745ffb943e4f
Tyler Chan <tylerchan28@gmail.com>
2021-04-30 17:19:42 -0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require("path");
-const { SourceMapDevToolPlugin } = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
-module.exports = {
+module.exports = (env) => {
+    const isProduction = env === "production";
+    const CSSExtract = new ExtractTextPlugin("styles.css");
+    
+    return {
     entry: "./src/app.js",
     output: {
         path: path.join(__dirname, "public"),
@@ -14,16 +18,31 @@ module.exports = {
             exclude: /node_modules/
         }, {
             test: /\.s?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ] //use allows an array of loaders
+            use: CSSExtract.extract({
+                use: [
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            })
         }]
     },
-    devtool: "cheap-module-eval-source-map",
+    plugins: [
+        CSSExtract
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
     devServer: {
         contentBase: path.join(__dirname, "public"),
         historyApiFallback: true //tells dev server we handle routing with client side code
     }
-}
+};
+};
